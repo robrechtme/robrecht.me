3084a4e4dc8489be331cc118da2c342ed3205069
robrechtme <robrechtmeersman@hotmail.com>
2021-11-11 10:53:18 +0100

    ✨ Move mail to new domain

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,31 +1,47 @@
 import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+import { createHTML } from './template';
 
-const { SMTP_USERNAME, SMTP_PASSWORD, SMTP_RECIPIENT } = process.env;
+const FOOTER = 'Robrecht Meersman, Vosselarestraat 61, 9850 Landegem<br>BTW BE 0761 397 342';
 
+// create reusable transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: process.env.SMTP_SERVER,
+  port: 465,
+  secure: true,
   auth: {
-    user: SMTP_USERNAME,
-    pass: SMTP_PASSWORD,
+    user: process.env.SMTP_USERNAME,
+    pass: process.env.SMTP_PASSWORD,
   },
 });
 
 const mailer = ({ senderMail, name, text }) => {
-  const from = name && senderMail ? `${name} <${senderMail}>` : `${name || senderMail}`;
+  const from = `Robrecht Meersman <${process.env.SMTP_RECIPIENT}>`;
+  const content = `Hi ${name}<br><br>Bedankt voor uw bericht! Ik probeer u zo spoedig mogelijk te antwoorden.<br><br>Bericht:<br><span style="font-style: italic">${text.replace(
+    /\\n/g,
+    '<br/>',
+  )}</span><br><br>Tot binnenkort<br><br>Robrecht Meersman`;
   const message = {
     from,
-    to: SMTP_RECIPIENT,
-    subject: `New message from ${name || senderMail}`,
-    text: `
-from: ${from}
-
-${text}
-`,
+    to: senderMail,
+    bcc: process.env.SMTP_RECIPIENT,
+    subject: `Bevestiging van uw bericht op robrecht.me`,
+    text: content,
+    html: createHTML(content, FOOTER),
     replyTo: from,
-  };
-  console.log('📬 Sending mail...');
+  } as Mail.Options;
+  console.log('📫 Sending mail...');
   return new Promise((resolve, reject) => {
-    transporter.sendMail(message, (error, info) => (error ? reject(error) : resolve(info)));
+    transporter.sendMail(message, (error, info) => {
+      if (error) {
+        console.log('📫 An error occurred!');
+        console.error(error);
+        reject(error);
+      } else {
+        console.log('📫 Sent!');
+        resolve(info);
+      }
+    });
   });
 };
 
--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -1,14 +1,18 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      // Server-side secrets
       DATOCMS_API_TOKEN: string;
       DATOCMS_PREVIEW_SECRET: string;
-      NEXT_PUBLIC_GA_TRACKING_ID: string;
       RECAPTCHA_SERVER_KEY: string;
-      NEXT_PUBLIC_RECAPTCHA_CLIENT_KEY: string;
-      SMTP_USERNAME: string;
       SMTP_PASSWORD: string;
       SMTP_RECIPIENT: string;
+      SMTP_SERVER: string;
+      SMTP_USERNAME: string;
+
+      // Client-side variables
+      NEXT_PUBLIC_GA_TRACKING_ID: string;
+      NEXT_PUBLIC_RECAPTCHA_CLIENT_KEY: string;
       NEXT_PUBLIC_URL: string;
     }
   }
