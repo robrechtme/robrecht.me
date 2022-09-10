d03869e78e47e8fa93a3d8cfe896cdfb72ee5c69
robrechtme <robrechtmeersman@hotmail.com>
2022-09-10 11:53:53 +0200

    feat: use next/future/image

--- src/components/atoms/Image.tsx
+++ src/components/atoms/Image.tsx
@@ -1,23 +1,34 @@
+import Image, { ImageLoader, ImageProps } from 'next/future/image';
+
 import { ResponsiveImageFieldsFragment } from 'lib/types';
 
-interface Props {
+const imgixLoader: ImageLoader = ({ quality, src, width }) => {
+  const url = new URL(src);
+  if (quality !== undefined) {
+    url.searchParams.set('q', String(quality));
+  }
+  if (width !== undefined) {
+    url.searchParams.set('w', String(width));
+  }
+  return url.toString();
+};
+
+interface Props extends Pick<ImageProps, 'priority' | 'className'> {
   data: ResponsiveImageFieldsFragment;
   className?: string;
 }
 
-const Img = ({ data, className }: Props) => (
-  <picture>
-    <source srcSet={data.webpSrcSet} sizes={data.sizes} type='image/webp' />
-    <source srcSet={data.srcSet} sizes={data.sizes} />
-    <img
-      width={data.width}
-      height={data.height}
-      className={className}
-      src={data.src}
-      alt={data.alt}
-      title={data.title}
-    />
-  </picture>
+const Img = ({ data, ...imageProps }: Props) => (
+  <Image
+    loader={imgixLoader}
+    src={data.src}
+    alt={data.alt}
+    width={data.width}
+    blurDataURL={data.base64}
+    height={data.height}
+    placeholder='blur'
+    {...imageProps}
+  />
 );
 
 export default Img;
